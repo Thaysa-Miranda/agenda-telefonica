cbb6b96b073be1bc671094693a5527c8c5a715df
thaysabmiranda <thaysa.mbmiranda@gmail.com>
2022-06-20 16:04:21 -0300

    atualizando

--- js/agenda.js
+++ js/agenda.js
@@ -1,16 +1,16 @@
 function adicionar() {
-  const name = document.querySelector("#name");
-  const email = document.querySelector("#email");
-  const telefone = document.querySelector("#telefone");
-  const nameContato = document.querySelector(".nomeContato");
-  const emailContato = document.querySelector(".emailContato");
-  const telContato = document.querySelector(".telContato");
-
-  const valueName = name.value;
-  const valueEmail = email.value;
-  const valueTelefone = telefone.value;
-
-  nameContato.innerHTML = `${valueName}`;
-  emailContato.innerHTML = `${valueEmail}`;
-  telContato.innerHTML = `${valueTelefone}`;
-}
+    const name = document.querySelector("#name");
+    const email = document.querySelector("#email");
+    const telefone = document.querySelector("#telefone");
+    const nameContato = document.querySelector(".nomeContato");
+    const emailContato = document.querySelector(".emailContato");
+    const telContato = document.querySelector(".telContato");
+  
+    const valueName = name.value;
+    const valueEmail = email.value;
+    const valueTelefone = telefone.value;
+  
+    nameContato.innerHTML = `${valueName}`;
+    emailContato.innerHTML = `${valueEmail}`;
+    telContato.innerHTML = `${valueTelefone}`;
+  }
